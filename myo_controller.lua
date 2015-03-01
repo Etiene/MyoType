a10afe4a38c0977739c3eb84de6c76e7fe8f56fc
Etiene Dalcol <dalcol@etiene.net>
2015-03-01 00:41:55 +0000

    Backspace

--- myo_controller.lua
+++ myo_controller.lua
@@ -1,5 +1,5 @@
-scriptId = 'com.thalmic.examples.outputeverything'
-scriptTitle = "Output Everything"
+scriptId = 'com.thalmic.examples.myotype'
+scriptTitle = "MyoType"
 scriptDetailsUrl = "" -- We don't have this until it's submitted to the Myo Market
 
 local right = false
@@ -9,11 +9,13 @@ function onPoseEdge(pose, edge)
     myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
     if pose == 'waveIn' and edge =='on' then
     	left = true
+        internal_clock = 0
     	myo.keyboard("left_arrow", "down")
     end
 
     if pose == 'waveOut' and edge =='on'  then
     	right = true
+        internal_clock = 0
     	myo.keyboard("right_arrow", "down")
     end
 
@@ -34,7 +36,7 @@ end
 function onPeriodic()
 	internal_clock = internal_clock + 10
 	myo.unlock()
-	if internal_clock == 1000 then
+	if internal_clock == 300 then
 		internal_clock = 0
 		if right == true then
 			myo.keyboard("right_arrow", "press")
